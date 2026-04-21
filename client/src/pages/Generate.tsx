220f26af04610472c5ffa21b2839fab47320f3b1
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-04-21 19:08:59 +0530

    Aspect Ratio Selector added

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import type { IThumbnail } from '../assets/assets';
+import { type AspectRatio, colorSchemes, type ThumbnailStyle, type IThumbnail } from '../assets/assets';
 import SoftBackdrop from '../components/SoftBackdrop';
+import AspectRatioSelector from '../components/AspectRatioSelector';
 
 const Generate = () => {
     
@@ -10,6 +11,10 @@ const Generate = () => {
     const [additionalDetails, setAdditionalDetails] = useState("");
     const [thumbnail, setThumbnail] = useState<IThumbnail | null>();
     const [loading, setLoading] = useState(false);
+    const [aspectRatio, setAspectRatio] = useState<AspectRatio>("16:9");
+    const [colorSchemeId, setColorSchemeId] = useState<string>(colorSchemes[0].id);
+    const [style, setStyle] = useState<ThumbnailStyle>("Bold & Graphic");
+    const [styleDropdownOpen, setStyleDropdownOpen] = useState(false);
     
     return (
         <>
@@ -44,6 +49,7 @@ const Generate = () => {
                                     </div>
 
                                     {/* Aspect Ratio Selector */}
+                                    <AspectRatioSelector value={aspectRatio} onChange={setAspectRatio} />
                                     {/* Style Selector */}
                                     {/* Color Scheme Selector */}
 
